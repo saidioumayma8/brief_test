39dea2120296c4a77b92f3e2a67aa80b471c6cb5
saidioumayma8 <oumaymasaidi908@gmail.com>
2023-11-24 11:45:41 +0100

    electronasr

--- adminh.php
+++ adminh.php
@@ -9,102 +9,271 @@
     <title>ElectroNaser</title>
 </head>
 <body>
-<div class="h2 text-center">ElectroNaser</div>
-
-
-
-<div class="container me-5" >
-      
-        <div class="row mb-5">
-          <div class="col-4 mb-5">
-          <div class="card" id="1" style="width: 350px; background: none; border:none">
-            <img class="card-img-top" src="image/tswira1.jpg" alt="Card image">
-            <div class="card-body text-center">
-              <h4 class="card-title">Tekka Maki</h4>
-              <p class="card-text">950 dh</p>
-              <button type="button" class="btn btn-danger m-l-" onclick="handlePersonalizeButtonClick(event, '1')">customize</button>
-              <button type="button" class="btn btn-success">add to cart</button>
-            </div>
-          </div>
-
-         
-          </div>
-
-
-          <div class="col-4 mb-5">
-            <div class="card" id="2" style="width:350px; background: none; border:none">
-                <img class="card-img-top" src="image/Rectangle 3.png" alt="Card image">
-                  <div class="card-body text-center">
-                    <h4 class="card-title">Texan roll</h4>
-                    <p class="card-text">700 dh</p>
-                    <button type="button" class="btn btn-danger" onclick="handlePersonalizeButtonClick(event, '2')">customize</button>
-                    <button type="button" class="btn btn-success">add to cart</button>
-                </div>
-            </div>
-        
-          </div>
-
-
-          <div class="col-4 mb-5">
-          <div class="card" id="3" style="width:350px; background: none; border:none">
-            <img class="card-img-top" src="image/tswira5.jpg" alt="Card image">
-            <div class="card-body text-center">
-              <h4 class="card-title">Shrimp Avocado Roll</h4>
-              <p class="card-text">850 dh</p>
-              <button type="button" class="btn btn-danger" onclick="handlePersonalizeButtonClick(event, '3')">customize</button>
-          <button type="button" class="btn btn-success">add to cart</button>
-            </div>
-          </div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a  class="h2 text-center mt-5 mb-4"href="#">ElectroNaser</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">log out</a>
+          </li>
           
-          </div>
+        </ul>
+      </div>
+    </div>
+  </nav>
 
 
-          <div class="col-4 mb-5">
-          <div class="card" id="4" style="width:350px; background: none; border:none">
-            <img class="card-img-top" src="image/Rectangle 9.png" alt="Card image">
-            <div class="card-body text-center">
-              <h4 class="card-title">Red Dragon Roll</h4>
-              <p class="card-text">990 dh</p>
-              <button type="button" class="btn btn-danger" onclick="handlePersonalizeButtonClick(event, '4')">customize</button>
-              <button type="button" class="btn btn-success">add to cart</button>
-            </div>
-          </div>
- 
+<form class="mb-4">
+      <div class="row">
+        <div class="col-md-6">
+          <label for="productCategory" class="form-label">Catégorie du produit :</label>
+          <select class="form-select" id="productCategory">
+            <option value="">Toutes les Produit</option>
+            <option value="category1">Catégorie 1</option>
+            <option value="category2">Catégorie 2</option>
+          </select>
         </div>
+      </div>
+    </form>
 
-
-        <div class="col-4 mb-5">
-          <div class="card" id="5" style="width:350px; background: none; border:none">
-            <img class="card-img-top" src="image/tswira4.jpg" alt="Card image">
-            <div class="card-body text-center">
-              <h4 class="card-title">Tiger Roll</h4>
-              <p class="card-text">920 dh</p>
-              <button type="button" class="btn btn-danger" onclick="handlePersonalizeButtonClick(event, '5')">customize</button>
-              <button type="button" class="btn btn-success">add to cart</button>
-            </div>
-          </div>
-
-        </div>
-
-
-        <div class="col-4 mb-5">
-          <div class="card" id="6" style="width:350px; background: none; border:none">
-            <img class="card-img-top" src="image/tswira5.jpg" alt="Card image">
-            <div class="card-body text-center">
-              <h4 class="card-title">Salmon Skin Roll</h4>
-              <p class="card-text">990 dh</p>
-              <button type="button" class="btn btn-danger" onclick="handlePersonalizeButtonClick(event, '6')">customize</button>
-              <button type="button" class="btn btn-success">add to cart</button>
-            </div>
-          </div>
+<div class="container me-5">
+  <div class="row g-3 mb-5">
+    <div class="col-4">
+      <div class="card" style="width: 18rem;">
+        <img src="image/tswira9.jpg" class="card-img-top" alt="Card image">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">1000 Dh</p>
+          <a href="#" class="btn btn-primary">Modifier</a>
+          <a href="#" class="btn btn-primary">supprimer</a>
 
         </div>
+      </div>
+    </div>
 
 
+      <div class="col-4">
+        <div class="card" style="width: 18rem;">
+          <img src="image/tswira8.jpg" class="card-img-top" alt="Card image">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">850 Dh</p>
+            <a href="#" class="btn btn-primary">Modifier</a>
+            <a href="#" class="btn btn-primary">supprime</a>
+
+          </div>
         </div>
+      </div>
+
+      <div class="col-4">
+        <div class="card" style="width: 18rem;">
+          <img src="image/tswira8.jpg" class="card-img-top" alt="Card image">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">745 Dh.</p>
+            <a href="#" class="btn btn-primary">Modifier</a>
+            <a href="#" class="btn btn-primary">supprime</a>
+
+          </div>
+        </div>
+      </div>
+
+
+      <div class="col-4">
+        <div class="card" style="width: 18rem;">
+          <img src="image/tswira8.jpg" class="card-img-top" alt="Card image">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">1000 Dh</p>
+            <a href="#" class="btn btn-primary">Modifier</a>
+            <a href="#" class="btn btn-primary">supprime</a>
+
+          </div>
+        </div>
+      </div>
+
+      <div class="col-4">
+        <div class="card" style="width: 18rem;">
+          <img src="image/tswira8.jpg" class="card-img-top" alt="Card image">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text"><p style="color: red;">800 Dh</p><s>950 Dh</s></p>
+            <a href="#" class="btn btn-primary">Modifier</a>
+            <a href="#" class="btn btn-primary">supprime</a>
+
+          </div>
+        </div>
+      </div>
+
+<div class="col-4">
+        <div class="card" style="width: 18rem;">
+          <img src="image/tswira8.jpg" class="card-img-top" alt="Card image">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">850 Dh</p>
+            <a href="#" class="btn btn-primary">Modifier</a>
+            <a href="#" class="btn btn-primary">supprime</a>
+          </div>
+        </div>
+      </div>
        
           
       </div>
+      <div class="container my-10">
+        <nav aria-label="page navigation example">
+          <ul class="pagination">
+              <li class="page-item"><a class="page-link" href="#"><span aria-hidden="true">&laquo;</span></a></li>
+              <li class="page-item"><a class="page-link" href="home.php"> 1</a></li>
+              <li class="page-item"><a class="page-link" href="home1.php"> 2</a></li>
+              <li class="page-item"><a class="page-link" href="#"><span aria-hidden="true">&raquo;</span></a></li>
+          </ul>
+        </nav>
+      </div>
+
+
+
+      <table class="table">
+  <caption>List of users</caption>
+  <thead>
+    <tr>
+      <th scope="col">Id</th>
+      <th scope="col">Email</th>
+      <th scope="col">password</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark@gmail.com</td>
+      <td>Otto</td>
+      <td><a href="#" class="btn btn-primary">Accepter</a><a href="#" class="btn btn-primary">suppremier</a></td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob@gmail.com</td>
+      <td>Thornton</td>
+      <td><a href="#" class="btn btn-primary">Accepter</a><a href="#" class="btn btn-primary">suppremier</a></td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry@gmail.com</td>
+      <td>the Bird</td>
+      <td><a href="#" class="btn btn-primary">Accepter</a><a href="#" class="btn btn-primary">suppremier</a></td>
+    </tr>
+  </tbody>
+</table>
+
+
+      <!-- Footer -->
+<footer class="text-center text-lg-start bg-body-tertiary text-muted">
+  <!-- Section: Social media -->
+  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <!-- Left -->
+    <div class="me-5 d-none d-lg-block">
+      <span class="h2 text-center mt-5 mb-4">ElectroNaser</span>
+    </div>
+    <!-- Left -->
+
+    <!-- Right -->
+    <div>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>ElectroNaser
+          </h6>
+          <p>
+            Here you can use rows and columns to organize your footer content. Lorem ipsum
+            dolor sit amet, consectetur adipisicing elit.
+          </p>
+        </div>
+        <!-- Grid column -->
+
+      
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Useful links
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Pricing</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Settings</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Orders</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Help</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+          <p><i class="fas fa-home me-3"></i> Rabat, NY 10012, ma</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            ElectroNaser@gmail.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i> + 212 64 567 88</p>
+          <p><i class="fas fa-print me-3"></i> + 212 64 567 89</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2021 Copyright:
+    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDElectroNaser.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 </body>
--- home.php
+++ home.php
@@ -9,7 +9,39 @@
     <title>ElectroNaser</title>
 </head>
 <body>
-<div class="h2 text-center mt-5 mb-4">ElectroNaser</div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a  class="h2 text-center mt-5 mb-4"href="#">ElectroNaser</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">log out</a>
+          </li>
+          
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+
+<form class="mb-4">
+      <div class="row">
+        <div class="col-md-6">
+          <label for="productCategory" class="form-label">Catégorie du produit :</label>
+          <select class="form-select" id="productCategory">
+            <option value="">Toutes les Produit</option>
+            <option value="category1">Catégorie 1</option>
+            <option value="category2">Catégorie 2</option>
+          </select>
+        </div>
+      </div>
+    </form>
 
 <div class="container me-5">
   <div class="row g-3 mb-5">
@@ -18,7 +50,7 @@
         <img src="image/tswira9.jpg" class="card-img-top" alt="Card image">
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
-          <p class="card-text"><p style="color: red;">850 Dh</p><s>1000 Dh</s></p>
+          <p class="card-text">1000 Dh</p>
           <a href="#" class="btn btn-primary">Buy Now</a>
         </div>
       </div>
@@ -30,7 +62,7 @@
           <img src="image/tswira8.jpg" class="card-img-top" alt="Card image">
           <div class="card-body">
             <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <p class="card-text">850 Dh</p>
             <a href="#" class="btn btn-primary">Buy Now</a>
           </div>
         </div>
@@ -41,7 +73,7 @@
           <img src="image/tswira8.jpg" class="card-img-top" alt="Card image">
           <div class="card-body">
             <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <p class="card-text">745 Dh.</p>
             <a href="#" class="btn btn-primary">Buy Now</a>
           </div>
         </div>
@@ -53,7 +85,7 @@
           <img src="image/tswira8.jpg" class="card-img-top" alt="Card image">
           <div class="card-body">
             <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <p class="card-text">1000 Dh</p>
             <a href="#" class="btn btn-primary">Buy Now</a>
           </div>
         </div>
@@ -64,7 +96,7 @@
           <img src="image/tswira8.jpg" class="card-img-top" alt="Card image">
           <div class="card-body">
             <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <p class="card-text"><p style="color: red;">800 Dh</p><s>950 Dh</s></p>
             <a href="#" class="btn btn-primary">Buy Now</a>
           </div>
         </div>
@@ -75,7 +107,7 @@
           <img src="image/tswira8.jpg" class="card-img-top" alt="Card image">
           <div class="card-body">
             <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <p class="card-text">850 Dh</p>
             <a href="#" class="btn btn-primary">Buy Now</a>
           </div>
         </div>
@@ -93,6 +125,112 @@
           </ul>
         </nav>
       </div>
+
+
+
+
+      <!-- Footer -->
+<footer class="text-center text-lg-start bg-body-tertiary text-muted">
+  <!-- Section: Social media -->
+  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <!-- Left -->
+    <div class="me-5 d-none d-lg-block">
+      <span class="h2 text-center mt-5 mb-4">ElectroNaser</span>
+    </div>
+    <!-- Left -->
+
+    <!-- Right -->
+    <div>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>ElectroNaser
+          </h6>
+          <p>
+            Here you can use rows and columns to organize your footer content. Lorem ipsum
+            dolor sit amet, consectetur adipisicing elit.
+          </p>
+        </div>
+        <!-- Grid column -->
+
+      
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Useful links
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Pricing</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Settings</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Orders</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Help</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+          <p><i class="fas fa-home me-3"></i> Rabat, NY 10012, ma</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>
+            ElectroNaser@gmail.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i> + 212 64 567 88</p>
+          <p><i class="fas fa-print me-3"></i> + 212 64 567 89</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2021 Copyright:
+    <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDElectroNaser.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 </body>
